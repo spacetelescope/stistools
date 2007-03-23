197a1ac2ebaea731602bc82ce881d45bdeb35e32
dencheva <dencheva@stsci.edu>
2007-03-23 20:09:04 +0000

    typo
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/stistools@695 fe389314-cf27-0410-b35b-8c050e845b92

--- test/mktrace-tests/plot_results_e1.py
+++ test/mktrace-tests/plot_results_e1.py
@@ -1,5 +1,5 @@
 """
-Plot results of basic test level 1
+Plot results of test with E1 aperture positions 
 input file: o8pp31020_crj.fits
 
 """
@@ -18,9 +18,10 @@ f=pyfits.open('o8pp31020_crj_1dt.fits)
 a2disp=f[1].data.field('A2DISPL')
 f.close()
 
+ax1=pylab.subplot(231)
 pylab.plot(a2disp[1792])
 pylab.plot(a2disp_ref[1792])
-
+pylab.title('trace tables')
 ###########################
 
 fref=pyfits.open('ref_o8pp31020_crj_1dt__interp.fits)
@@ -30,10 +31,10 @@ fref.close()
 f=pyfits.open('o8pp31020_crj_1dt_interp.fits)
 data_ref=f[0].data
 f.close()
-
+ax1=pylab.subplot(232)
 pylab.plot(data_ref)
 pylab.plot(data)
-
+pylab.title('interpolated trace')
 ##############################
 
 fref=pyfits.open('ref_o8pp31020_crj_1dt__interpfit.fits)
@@ -43,10 +44,10 @@ fref.close()
 f=pyfits.open('o8pp31020_crj_1dt_interpfit.fits)
 data_ref=f[0].data
 f.close()
-
+ax1=pylab.subplot(233)
 pylab.plot(data_ref)
 pylab.plot(data)
-
+pylab.title('fit to interp trace')
 
 
 ##############################
@@ -58,10 +59,10 @@ fref.close()
 f=pyfits.open('o8pp31020_crj_1dt_sci.fits)
 data_ref=f[0].data
 f.close()
-
+ax1=pylab.subplot(234)
 pylab.plot(data_ref)
 pylab.plot(data)
-
+pylab.title('science trace')
 
 ##############################
 
@@ -72,7 +73,8 @@ fref.close()
 f=pyfits.open('o8pp31020_crj_1dt_scifit.fits)
 data_ref=f[0].data
 f.close()
-
+ax1=pylab.subplot(235)
 pylab.plot(data_ref)
 pylab.plot(data)
-
+pylab.title('fit to sci trace ')
+pylab.show()
