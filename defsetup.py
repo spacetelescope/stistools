cbc5000d565d48addfb4e0480f7aaa5aaa588c31
sienkiew <sienkiew@stsci.edu>
2008-08-14 21:42:15 +0000

    setup.py mods - more changes
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/stistools@6930 fe389314-cf27-0410-b35b-8c050e845b92

---
+++ defsetup.py
@@ -0,0 +1,13 @@
+pkg =  "stistools"
+
+setupargs =	{
+    'version' :		    "1.0.0",
+    'description' :		"Python Tools for STIS Data",
+    'author' :		    "Paul Barrett, Phil Hodge",
+    'author_email' :	"help@stsci.edu",
+    'license' :		    "http://www.stsci.edu/resources/software_hardware/pyraf/LICENSE",
+    'platforms' :		["Linux","Solaris","Mac OS X","Win"],
+
+}
+
+
