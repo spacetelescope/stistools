5f7267fd81d3f383d299d7bdfa796585db3846cc
sienkiew <sienkiew@stsci.edu>
2009-03-26 19:55:08 +0000

    fix to use defsetup
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/stistools@7817 fe389314-cf27-0410-b35b-8c050e845b92

--- setup.py
+++ setup.py
@@ -1,30 +1,4 @@
-from distutils.core import setup
-import sys, os.path
+#!/usr/bin/env python
 
-if not hasattr(sys, 'version_info') or sys.version_info < (2,3,0,'alpha',0):
-    raise SystemExit, "Python 2.3 or later required to build stistools."
-
-args = sys.argv[:]
-
-for a in args:
-    if a.startswith('--local='):
-        dir = os.path.abspath(a.split("=")[1])
-        sys.argv.extend([
-                "--install-lib="+dir,
-                "--install-scripts=%s" % dir])
-        args.remove(a)
-        sys.argv.remove(a)
-
-setup(name = "stistools",
-      version = "1.0.0",
-      description = "Python Tools for STIS Data",
-      author = "Paul Barrett, Phil Hodge",
-      author_email = "help@stsci.edu",
-      license = "http://www.stsci.edu/resources/software_hardware/pyraf/LICENSE",
-      platforms = ["Linux","Solaris","Mac OS X","Win"],
-      packages = ['stistools'],
-      package_dir = {'stistools':'lib'}
-      )
-
-
-
+import pytools.stsci_distutils_hack
+pytools.stsci_distutils_hack.run(pytools_version = "3.0")
