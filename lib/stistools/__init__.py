54a4f5ef6c75f8d1d5833bafdfb59a3243397cb0
sienkiew <sienkiew@stsci.edu>
2013-02-06 20:29:07 +0000

    get version from new install
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stistools/trunk@22263 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/stistools/__init__.py
+++ lib/stistools/__init__.py
@@ -1,11 +1,4 @@
-try:
-    from stistools.svninfo import(__version__, __svn_revision__,
-                                  __svn_full_info__, __setup_datetime__)
-except ImportError:
-    __version__ = '1.0.0'
-    __svn_revision__ = ''
-    __svn_full_info__ = ''
-    __setup_datetime__ = None
+from .version import *
 
 import calstis
 import basic2d
