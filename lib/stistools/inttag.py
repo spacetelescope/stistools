daf4efa5cfcaf4174b2309448564552e5acae4a0
Doug Branton <dbranton@stsci.edu>
2018-05-23 11:50:46 -0400

    Corrected GTI mapping

--- lib/stistools/inttag.py
+++ lib/stistools/inttag.py
@@ -2,17 +2,17 @@
 import numpy as np
 from astropy.io import fits
 import astropy.stats
-import matplotlib.pyplot as plt
+from astropy import units as u
 
 
-def inttag(input, output, starttime=None, increment=None,
-            rcount=1, highres=False, allevents=False, verbose=True):
+def inttag(tagfile, output, starttime=None, increment=None,
+           rcount=1, highres=False, allevents=False, verbose=True):
     """Convert an events table of TIMETAG into an integrated ACCUM image.
 
         Parameters
         ----------
-        input: str
-            nput file that contains TIMETAG event stream. This is ordinarily a
+        tagfile: str
+            input file that contains TIMETAG event stream. This is ordinarily a
             FITS file containing two tables. The TIMETAG data are in the table
             with EXTNAME = "EVENTS", and the "good time intervals" are in the
             table with EXTNAME = "GTI". If the GTI table is missing or empty,
@@ -32,8 +32,7 @@ def inttag(input, output, starttime=None, increment=None,
 
         rcount: int
             Repeat count, the number of output image sets to create. If rcount is
-            greater than 1 then increment must also be specified (but starttime may
-            still be set to None).
+            greater than 1 and increment is not specified, will subdivide the total exposure time by rcount.
 
         highres: bool
             Create a high resolution output image? Default is False.
@@ -52,7 +51,7 @@ def inttag(input, output, starttime=None, increment=None,
 
 """
     # Open Input File (_tag.fits)
-    tag_hdr = fits.open(input)
+    tag_hdr = fits.open(tagfile)
 
     # Read in GTI (Good Time Interval) Table
     gti_data = tag_hdr['GTI'].data
@@ -109,10 +108,8 @@ def inttag(input, output, starttime=None, increment=None,
     for imset in range(rcount):
 
         # Get Exposure Times
-        #exp_time, expstart, expstop = exp_range(starttime, stoptime, gti_data, tzero_mjd)
-        exp_time, expstart, expstop, good_events = exp_range_py(starttime, stoptime, events_data, gti_data, tzero_mjd)
+        exp_time, expstart, expstop, good_events = exp_range(starttime, stoptime, events_data, gti_data, tzero_mjd)
 
-        #if exp_time <= 0.:
         if len(good_events) == 0:
             if verbose:
                 print("Skipping imset, due to no overlap with GTI\n", starttime, stoptime)
@@ -136,9 +133,9 @@ def inttag(input, output, starttime=None, increment=None,
         accum = events_to_accum(good_events, siz_axx, siz_axy, highres)
 
         # Calculate errors from accum image
-        # Note: C takes the square root of the counts, inttag.py uses a more robust confidence interval
+        # Note: C version takes the square root of the counts, inttag.py uses a more robust confidence interval
         conf_int = astropy.stats.poisson_conf_interval(accum, interval='sherpagehrels', sigma=1)
-        err = conf_int[1] - accum  # Error is the difference between upper confidence boundary and the data
+        err = conf_int[1] - accum  # error is the difference between upper confidence boundary and the data
 
         # Copy EVENTS extension header to SCI, ERR, DQ extensions
         sci_hdu = fits.ImageHDU(data=accum, header=tag_hdr[1].header.copy(), name='SCI')
@@ -156,6 +153,7 @@ def inttag(input, output, starttime=None, increment=None,
             keyword_list = list(hdu.header.keys())
             if not any("CTYPE" in keyword for keyword in keyword_list):
                 n, k = [keyword[-1] for keyword in keyword_list if "TCTYP" in keyword]
+
                 # Rename keywords
                 for val, i in zip([n, k], ['1', '2']):
                     hdu.header.rename_keyword('TCTYP' + val, 'CTYPE' + i)
@@ -174,6 +172,7 @@ def inttag(input, output, starttime=None, increment=None,
             hdu.header['LTV1'] = ltvx
             hdu.header['LTV2'] = ltvy
 
+            # Convert keyword values to lowres scale if not highres
             if not highres:
                 pass
                 hdu.header['CD1_1'] *= 2
@@ -183,20 +182,25 @@ def inttag(input, output, starttime=None, increment=None,
                 hdu.header['CRPIX1'] = (hdu.header['CRPIX1'] + 0.5) / 2.
                 hdu.header['CRPIX2'] = (hdu.header['CRPIX2'] + 0.5) / 2.
 
+        # Append imset extensions to header list
         hdu_list.append(sci_hdu)
         hdu_list.append(err_hdu)
         hdu_list.append(dq_hdu)
 
+        # Prepare start and stop times for next image in imset
         starttime = stoptime
         stoptime += increment
         texptime += exp_time
 
+    # Copy tag file primary header to output header
     pri_hdu = fits.PrimaryHDU(header=tag_hdr[0].header.copy())
     pri_hdu.header['NEXTEND'] = imset_hdr_ver * 3  # Three extensions per imset (SCI, ERR, DQ)
     pri_hdu.header['NRPTEXP'] = imset_hdr_ver
     pri_hdu.header['TEXPSTRT'] = texpstart
     pri_hdu.header['TEXPEND'] = texpend
     pri_hdu.header['TEXPTIME'] = texptime
+    if not highres:
+        pri_hdu.header['LORSCORR'] = "COMPLETE"  # Corr flag detailing MAMA data conversion to low res
 
     # Write output file
     hdu_list = [pri_hdu] + hdu_list
@@ -204,52 +208,35 @@ def inttag(input, output, starttime=None, increment=None,
     out_hdul.writeto(output, overwrite=True)
 
 
-def exp_range(starttime, stoptime, gti_data, tzero_mjd):
-    """Calculate exposure time, expstart, and expstop """
-    start_times = gti_data['START']
-    stop_times = gti_data['STOP']
-    sec_per_day = 86400
+def exp_range(starttime, stoptime, events_data, gti_data, tzero_mjd):
+    """Calculate exposure time, expstart, and expstop and mask imset
 
-    # Figure out start and stop times from GTI(s)
-    if (stoptime < start_times[0]) or (starttime > stop_times[-1]):  # Outside total GTI Range
-        exp_time = 0
-        expstart = tzero_mjd
-        expstop = tzero_mjd
-        return exp_time, expstart, expstop
+        Parameters
+        ----------
+        starttime: float
+            Start time of the imset in seconds
 
-    exp_time = 0
-    for i in range(len(gti_data)):  # For each GTI
-        if (stoptime < start_times[i]) or (starttime > stop_times[i]):  # Not in GTI
-            continue
-        start_expt = max([start_times[i], starttime])
-        stop_expt = min([stop_times[i], stoptime])
-        exp_time += (stop_expt - start_expt)
-    if exp_time <= 0.0:
-        expstart = tzero_mjd
-        expstop = tzero_mjd
-        return exp_time, expstart, expstop
+        stoptime: float
+            Stop time of the imset in seconds
 
-    # The following assumes the GTI are sorted by time
-    for i in range(len(gti_data)):  # For each GTI
-        if (starttime >= start_times[i]) and (starttime <= stop_times[i]):  # Starts within GTI
-            start_expt = starttime
-        elif starttime <= start_times[i]:  # Starts before GTI
-            start_expt = start_times[i]
-    expstart = tzero_mjd + start_expt/sec_per_day
+        events_data: record array
+            Record array of timetag events.
 
-    for i in range(len(gti_data)):  # For each GTI
-        if (stoptime >= start_times[i]) and stoptime <= stop_times[i]:  # Stops within GTI
-            stop_expt = stoptime
-        elif stoptime > stop_times[i]:  # Stops after GTI, different from C code, write a test for this
-            stop_expt = stop_times[i]
-    expstop = tzero_mjd + stop_expt/sec_per_day
-    return exp_time, expstart, expstop
+        gti_data: record array
+            Record array of good time intervals (GTIs).
+
+        tzero_mjd: bool
+            Modified Julian Date (MJD) corresponding to the beginning of the exposure
+
+        Returns
+        -------
+        exp_time:
 
 
-def exp_range_py(starttime, stoptime, events_data, gti_data, tzero_mjd):
-    """Calculate exposure time, expstart, and expstop and mask imset"""
-    sec_per_day = 86400
+"""
+    sec_per_day = (1*u.day).to(u.second).value
     imset_events = events_data[(events_data['TIME'] > starttime) * (events_data['TIME'] < stoptime)]  # within imset
+
     if len(imset_events) == 0:  # No events in imset
         exp_time = 0
         expstart = tzero_mjd
@@ -257,38 +244,90 @@ def exp_range_py(starttime, stoptime, events_data, gti_data, tzero_mjd):
         return exp_time, expstart, expstop, imset_events
 
     # Mask events in imset if there are any lapses in GTI
-    gti_mask = [True] * len(imset_events)
-    masked_time = 0
+    gti_mask = np.array([False] * len(imset_events))  # Start by assuming all events are outside all GTIs
     for gti in gti_data:
+        # Create mask of events within GTI
         mask = (imset_events['TIME'] > gti[0]) * (imset_events['TIME'] < gti[1])
-        gti_mask *= mask
-        masked_events = imset_events[~gti_mask]
+        gti_mask = np.logical_or(gti_mask, mask)  # OR global gti mask with local gti mask
 
-        # Track exposure time lost due to non-GTI
-        if len(masked_events) > 0:
-            masked_time += masked_events['TIME'][-1] - masked_events['TIME'][0]
+
 
     good_events = imset_events[gti_mask]  # All events in the imset within the GTI(s)
-    expstart = tzero_mjd + good_events['TIME'][0]/sec_per_day  # exposure start in MJD for imset
+    if len(good_events) == 0:
+        exp_time = 0
+        expstart = tzero_mjd
+        expstop = tzero_mjd
+        return exp_time, expstart, expstop, imset_events
+    bad_events = imset_events[~gti_mask]
+    expstart = tzero_mjd + good_events['TIME'][0] / sec_per_day  # exposure start in MJD for imset
     expstop = tzero_mjd + good_events['TIME'][-1] / sec_per_day  # exposure stop in MJD for imset
-    exp_time = imset_events['TIME'][-1] - imset_events['TIME'][0] - masked_time  # exposure time in seconds
+
+    exptime_loss = 0
+    gaps = []
+    if len(gti_data) > 1:
+        for i, gti in enumerate(gti_data):
+            if i == 0:
+                continue
+            gaps.append((gti_data[i - 1][1], gti[0]))
+    exptime_loss = 0
+    print(starttime, stoptime, gaps)
+    for gap in gaps:
+        if gap[1] < stoptime and gap[0] > starttime:
+            exptime_loss += gap[1] - gap[0]
+        elif gap[1] > stoptime and gap[0] < stoptime:
+            exptime_loss += stoptime - gap[0]
+        elif gap[1] > starttime and gap[0] < starttime:
+            exptime_loss += gap[1] - starttime
+        else:
+            continue
+
+    print(exptime_loss)
+    exp_time = stoptime - starttime - exptime_loss  # exposure time in seconds
 
     return exp_time, expstart, expstop, good_events
 
 
-def events_to_accum(events_table, size_x, size_y, highres):
-    print(size_x, size_y)
-    axis1 = events_table['AXIS1']
-    axis2 = events_table['AXIS2']
+def events_to_accum(events_data, size_x, size_y, highres):
+    """Map timetag events to a 2d accum image array.
+
+        Parameters
+        ----------
+        events_data: record array
+            Record array of timetag events.
+
+        size_x: int
+            Number of pixels on axis 1 of the detector.
+
+        size_y: int
+            Number of pixels on axis 2 of the detector.
+
+        highres: bool
+            Boolean value indicating whether the output accum image is in high or low resolution.
+
+        Returns
+        -------
+        accum: array
+            2d image of all events in the imset on the detector.
+
+
+"""
+    # Extract (x,y) event locations from events_table
+    axis1 = events_data['AXIS1']
+    axis2 = events_data['AXIS2']
+
+    # Determine resolution-appropriate binning
     if highres:
         range_y = size_y
         range_x = size_x
     else:
         range_y = size_y * 2
         range_x = size_x * 2
+
+    # Map events to an accum image using a 2d histogram
     accum, xedges, yedges = np.histogram2d(axis2, axis1, bins=[size_y, size_x], range=[[1, range_y], [1, range_x]])
+
     return accum
 
 
 if __name__ == "__main__":
-    inttag("ob3001xqq_tag.fits", "test.fits", rcount=3, verbose=True, highres=False)
+    inttag("gtigap_tag.fits", "test.fits", increment=50, rcount=24, verbose=True, highres=False)
