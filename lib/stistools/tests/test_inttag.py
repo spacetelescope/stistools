803f1ed848c1e1e0fbd2e071ea28a626c1c89ae3
Doug Branton <dbranton@stsci.edu>
2018-06-21 13:13:42 -0400

    corrected pathing

--- lib/stistools/tests/test_inttag.py
+++ lib/stistools/tests/test_inttag.py
@@ -5,12 +5,11 @@ import pytest
 
 threshold = 0.0001  # Acceptable ratio of different pixels to identical pixels
 
-
 def test_accum_lores():
     """Compare accum image output for a single lowres imset"""
-    iraf_file = 'data/lores.fits'
-    output = "test.fits"
-    inttag("data/oddv01050_tag.fits", output, highres=False)
+    iraf_file = 'data/inttag/lores.fits'
+    output = "data/inttag/test.fits"
+    inttag("data/inttag/oddv01050_tag.fits", output, highres=False)
     iraf_data = fits.open(iraf_file)[1].data
     output_data = fits.open(output)[1].data
 
@@ -19,9 +18,9 @@ def test_accum_lores():
 
 def test_accum_hires():
     """Compare accum image output for a single highres imset"""
-    iraf_file = 'data/hires.fits'
-    output = "test.fits"
-    inttag("data/oddv01050_tag.fits", output, highres=True)
+    iraf_file = 'data/inttag/hires.fits'
+    output = "data/inttag/test.fits"
+    inttag("data/inttag/oddv01050_tag.fits", output, highres=True)
     iraf_data = fits.open(iraf_file)[1].data
     output_data = fits.open(output)[1].data
 
@@ -30,9 +29,9 @@ def test_accum_hires():
 
 def test_accum_gtigap():
     """Compare accum image output for a single imset with a GTI gap"""
-    iraf_file = 'data/gtigap.fits'
-    output = "test.fits"
-    inttag("data/gtigap_tag.fits", output, highres=False)
+    iraf_file = 'data/inttag/gtigap.fits'
+    output = "data/inttag/test.fits"
+    inttag("data/inttag/gtigap_tag.fits", output, highres=False)
     iraf_data = fits.open(iraf_file)[1].data
     output_data = fits.open(output)[1].data
 
@@ -41,9 +40,9 @@ def test_accum_gtigap():
 
 def test_accum_allevents():
     """Compare accum image output in allevents mode"""
-    iraf_file = 'data/allevents.fits'
-    output = "test.fits"
-    inttag("data/gtigap_tag.fits", output, highres=False, allevents=True)
+    iraf_file = 'data/inttag/allevents.fits'
+    output = "data/inttag/test.fits"
+    inttag("data/inttag/gtigap_tag.fits", output, highres=False, allevents=True)
     iraf_data = fits.open(iraf_file)[1].data
     output_data = fits.open(output)[1].data
 
@@ -52,10 +51,10 @@ def test_accum_allevents():
 
 def test_accum_rcount():
     """Compare accum image multi-imset output"""
-    iraf_file = 'data/rcount.fits'
-    output = "test.fits"
-    inttag("data/gtigap_tag.fits", output, starttime=700, increment=200, rcount=5, highres=False)
-    n_sci = int(fits.open("data/rcount.fits")[0].header['NEXTEND'] / 3)
+    iraf_file = 'data/inttag/rcount.fits'
+    output = "data/inttag/test.fits"
+    inttag("data/inttag/gtigap_tag.fits", output, starttime=700, increment=200, rcount=5, highres=False)
+    n_sci = int(fits.open("data/inttag/rcount.fits")[0].header['NEXTEND'] / 3)
     for sci in range(n_sci):
         iraf_data = fits.open(iraf_file)['SCI', sci+1].data
         output_data = fits.open(output)['SCI', sci+1].data
