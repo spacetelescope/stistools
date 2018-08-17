05f15f65a24bff2d444d091d0fbc2bc4c5760279
Sara Ogaz <ogaz@stsci.edu>
2018-08-17 13:26:48 -0400

    all tests written and running, except for wx2d that needs fix, putting into separate commit

---
+++ tests/test_stisnoise.py
@@ -0,0 +1,49 @@
+from stistools.stisnoise import stisnoise
+from .resources import BaseSTIS
+from .helpers.mark import require_bigdata
+
+
+@require_bigdata
+class TestStisnoise(BaseSTIS):
+
+    input_loc = 'stisnoise'
+    ref_loc = 'stisnoise'
+
+    # Really should add some output and return array checks for these tests
+
+    def test_stisnoise_t1(self, capsys):
+        """
+        stisnoise with boxcar test and check output
+        """
+
+        # Prepare input files.
+        self.get_input_file("input", "o6ih10060_crj.fits")
+
+        capsys.readouterr()
+
+        # Run stisnoise
+        stisnoise('o6ih10060_crj.fits', outfile='o6ih10060_fcrj1.fits',
+                  boxcar=5)
+
+        # Compare results
+        captured = capsys.readouterr()
+        assert captured.out == "Target: V1016-CYG, Amp: D, Gain: 1\n"
+
+        outputs = [("o6ih10060_fcrj1.fits", "o6ih10060_fcrj1_ref.fits")]
+        self.compare_outputs(outputs)
+
+    def test_stisnoise_t2(self):
+        """
+        stisnoise with window test
+        """
+
+        # Prepare input files.
+        self.get_input_file("input", "o6ih10060_crj.fits")
+
+        # Run stisnoise
+        stisnoise('o6ih10060_crj.fits', outfile='o6ih10060_fcrj2.fits',
+                  window=[0.5, 0.5, 0.1])
+
+        # Compare results
+        outputs = [("o6ih10060_fcrj2.fits", "o6ih10060_fcrj2_ref.fits")]
+        self.compare_outputs(outputs)
---
+++ tests/test_x1d.py
@@ -0,0 +1,25 @@
+from stistools.x1d import x1d
+from .resources import BaseSTIS
+from .helpers.mark import require_bigdata
+
+
+@require_bigdata
+class TestX1d(BaseSTIS):
+
+    input_loc = 'x1d'
+    ref_loc = 'x1d/ref'
+
+    def test_x1d(self):
+        """
+        Basic x1d test, mostly using default parameters
+        """
+
+        # Prepare input files.
+        self.get_input_file("input", "o56j02020_flt.fits")
+
+        # Run basic2d
+        x1d("o56j02020_flt.fits", output="x1d_lev3.fits", ctecorr="omit")
+
+        # Compare results
+        outputs = [("x1d_lev3.fits", "x1d_lev3_ref.fits")]
+        self.compare_outputs(outputs)
