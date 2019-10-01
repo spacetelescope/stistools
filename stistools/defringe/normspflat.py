be1ab7e9d6b2f6d7023cccdcae6b9e01fe16e2dd
Sean Lockwood <lockwood@stsci.edu>
2019-10-01 19:02:21 -0400

    Completed do_cal section of normspflat, some manual testing performed.

--- stistools/defringe/normspflat.py
+++ stistools/defringe/normspflat.py
@@ -2,14 +2,22 @@
 
 import os
 import numpy as np
+import warnings
 from astropy.io import fits
 
 from ..r_util import expandFileName
 from ..calstis import calstis
 
 
-def normspflat(inflat, outflat, do_cal=True, biasfile=None, darkfile=None, pixelflat=None, 
-               wavecal=None):
+# Keyword choices for calstis reduction:
+PERFORM = {
+    'ALL':   ['DQICORR', 'BLEVCORR', 'BIASCORR', 'DARKCORR', 'FLATCORR', 'CRCORR'],
+    'G750M': ['WAVECORR', 'HELCORR', 'X2DCORR'],
+    'G750L': [],}
+OMIT_G750L = PERFORM['G750M'][:]
+
+def normspflat(inflat, outflat='.', do_cal=True, biasfile=None, darkfile=None,
+               pixelflat=None, wavecal=None):
     """Normalize STIS CCD fringe flat.
 
     Based on PyRAF `stsdas.hst_calib.stis.normspflat task 
@@ -20,7 +28,7 @@ def normspflat(inflat, outflat, do_cal=True, biasfile=None, darkfile=None, pixel
     inflat: str
         Name of input fringe flat
     outflat: str
-        Name of normalized fringe flat output
+        Name of normalized fringe flat output or directory location.  (Default=".")
     do_cal: bool
         Perform bias and dark subtraction and CR rejection?  (Default=True)
     biasfile: str or None
@@ -41,39 +49,111 @@ def normspflat(inflat, outflat, do_cal=True, biasfile=None, darkfile=None, pixel
     """
     # These notes are based on the old STSDAS algorithm.
 
-    # strip extension from rootname of inflat (assumed to be {RAW, CRJ, SX2, X2D, clff})
+    # Determine properties of inflat (assumed to be {RAW, CRJ, SX2, X2D, clff}):
+    inflat = os.path.abspath(inflat)
+    if not os.access(inflat, os.R_OK|os.W_OK):
+        raise FileNotFoundError('Cannot access inflat:  {}'.format(inflat))
+    hdr0 = fits.getheader(inflat, ext=0)
+    rootname = hdr0['ROOTNAME']  # *** Or use something derived from os.path.basename(inflat)? ***
+    opt_elem = hdr0['OPT_ELEM'].upper()
+    if opt_elem not in ['G750L', 'G750M']:
+        raise ValueError('Unsupported opt_elem="{}"!'.format(opt_elem))
 
-    # identify wavecal
+    # *** TO DO:  Confirm inflat has >= 2 SCI exts to allow CR-rejection ***
+    # *** TO DO:  Handle NRPTEXP keyword ***
 
-    # handle outflat filename formatting (e.g. a directory is given)
+    # Identify wavecal:
+    if opt_elem == 'G750M':
+        wavecal = wavecal or expandFileName(hdr0['WAVECAL'])
+        if not os.access(wavecal, os.R_OK):
+            raise FileNotFoundError('Cannot find G750M wavecal:  {}'.format(wavecal))
+    if wavecal in [None, 'N/A', 'n/a', '']:
+        wavecal = ''  # Format for non-specified wavecals going into calstis API
+    else:
+        wavecal = os.path.abspath(wavecal)
 
-    # if do_cal:
-    #    identify biasfile, darkfile, pixelflat
-    #    confirm inflat has >= 2 SCI exts to allow CR-rejection
-    #    handle NRPTEXP keyword
-    #    PERFORM:  DQICORR, BLEVCORR, BIASCORR, DARKCORR, FLATCORR, CRCORR
-    #    Update BIASFILE, DARKFILE, PFLTFILE
-    #    if G750M:
-    #        PERFORM:  WAVECORR, HELCORR, X2DCORR
-    #    elif G750L:
-    #        OMIT:  WAVECORR, HELCORR, X2DCORR
-    #    call stistools.calstis.calstis(inflat, wavecal, ...)
-    # 
-    #    perform pixel-to-pixel flat-fielding:
-    #        if G750M:
-    #            make unity file from inflat's SX2 (trivial flat)
-    #        elif G750L:
-    #            make unity file from inflat's CRJ --> tmp
-    #            copy CRJ to clff
+    # Handle outflat filename formatting:
+    outflat = os.path.abspath(outflat)
+    if os.path.isdir(outflat):
+        outflat = os.path.join(outflat, '{}_nsp.fits'.format(rootname))
+    if not os.access(os.path.dirname(outflat), os.W_OK):
+        raise IOError('Do not have permission to write to:  {}'.format(
+                      os.path.dirname(outflat)))
 
-    # elif not do_cal:
-    #    if G750M:
-    #        make unity file from inflat's RAW --> tmp
-    #    elif G750L:
-    #        make unity file from inflat's RAW --> tmp
-    #        copy inflat's RAW --> clff
+    perform = PERFORM['ALL'] + PERFORM[opt_elem]
+    if do_cal:
+        # Resolve reference files from supplied variables or header values:
+        ref_types = {
+            'BIASFILE': os.path.abspath(biasfile  or expandFileName(hdr0['BIASFILE'])),
+            'DARKFILE': os.path.abspath(darkfile  or expandFileName(hdr0['DARKFILE'])),
+            'PFLTFILE': os.path.abspath(pixelflat or expandFileName(hdr0['PFLTFILE'])),}
 
-    # Check that G750M inflats are either sx2 or x2d.
+        # Populate/repopulate the inflat header accordingly:
+        for i, (ref_type, ref) in enumerate(ref_types.items()):
+            if not os.access(ref, os.F_OK):
+                raise FileNotFoundError('Cannot access reference file:  {}'.format(ref))
+
+            # Handle reference file paths via environment variables:
+            ref_var = 'reff{:.0f}'.format(i+1)
+            os.environ[ref_var] = os.path.abspath(os.path.dirname(ref)) + os.sep
+            # Keep $oref where it's the same:
+            if os.path.normpath(os.environ[ref_var]) == os.path.normpath(os.environ['oref']):
+                ref_var = 'oref'
+                #os.environ['oref'] = os.path.abspath(os.environ['oref'])  # for when we cd
+            with fits.open(inflat, 'update') as f:
+                if ref_var == 'oref':
+                    f[0].header[ref_type] = '{}${}'.format(ref_var, os.path.basename(ref))
+                else:
+                    f[0].header[ref_type] = '${}/{}'.format(ref_var, os.path.basename(ref))
+
+        # Update calibration flags prior to calling calstis:
+        with fits.open(inflat, 'update') as f:
+            for keyword in perform:
+                if f[0].header[keyword] != 'COMPLETE':
+                    f[0].header[keyword] = 'PERFORM'
+            if opt_elem == 'G750M':
+                output_suffix = 'sx2'  # geometrically-corrected
+            elif opt_elem == 'G750L':
+                output_suffix = 'crj'  # not geometrically-corrected
+                for keyword in OMIT_G750L:
+                    if f[0].header[keyword] == 'PERFORM':
+                        f[0].header[keyword] = 'OMIT'
+
+        # Call calstis:
+        old_cwd = os.getcwd()  # outflat and ref_vars are abs paths
+        try:
+            os.chdir(os.path.dirname(inflat))  # Must be in same directory to pick up EPC file
+            trailer = os.path.join(os.path.dirname(outflat),
+                                   '{}_calstis.log'.format(rootname))
+            outroot = os.path.dirname(outflat) + os.sep
+            outname = os.path.join(os.path.dirname(outflat),
+                                   '{}_{}.fits'.format(rootname, output_suffix))
+            # Remove files from previous iterations of this script:
+            for old_file in [outname, trailer]:
+                if os.access(old_file, os.F_OK):
+                    os.remove(old_file)
+            res = calstis(os.path.basename(inflat), wavecal=wavecal, outroot=outroot,
+                          trailer=trailer)
+            if res != 0:
+                raise Exception('CalSTIS returned non-zero code:  {}\n' \
+                                'See log for more details:  {}'.format(res, trailer))
+            print ('File written:  {}'.format(outname))
+        finally:
+            os.chdir(old_cwd)
+    else:  # not do_cal
+        outname = inflat
+
+        # Check if user-supplied inflat has all the recommended calibrations performed:
+        keyword_warnings = []
+        for keyword in perform:
+            if hdr0[keyword] != 'COMPLETE':
+                keyword_warnings.append(keyword)
+        if keyword_warnings:
+            warnings.warn('These calibration steps should be COMPLETE:\n{}'.format(
+                ', '.join(keyword_warnings)))
+
+    # Read in the calibrated flat data:
+    data = fits.getdata(outname, ext=1)
 
     # Do a line-by-line cubic spline fit to the fringe flat to remove the lamp function...
 
@@ -103,7 +183,8 @@ def call_normspflat():
     parser = argparse.ArgumentParser(
         description='Normalize STIS CCD fringe flat')
     parser.add_argument('inflat', type=str, help='Name of input fringe flat')
-    parser.add_argument('outflat', type=str, help='Name of normalized fringe flat output')
+    parser.add_argument('--outflat', '-o', type=str, default='.',
+        help='Name of normalized fringe flat output or directory location (default=".")')
     parser.add_argument('--skip_cal', '-s', dest='do_cal', action='store_false', 
         help='Skip bias and dark subtraction and CR rejection?')
     parser.add_argument('--biasfile', '-b', type=str,
