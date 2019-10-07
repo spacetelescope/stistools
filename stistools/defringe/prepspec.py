1d502a4d67f4e0394e9700fcdb03935b47aceb55
mmaclay <mtmaclay@gmail.com>
2019-10-07 16:21:30 -0400

    added prepspec code from grit

--- stistools/defringe/prepspec.py
+++ stistools/defringe/prepspec.py
@@ -1,13 +1,19 @@
 #! /usr/bin/env python
 
 import os
+import re
 import numpy as np
+import argparse
+import textwrap
 from astropy.io import fits
+import stistools
 
-from ..r_util import expandFileName
-from ..basic2d import basic2d
-from ..x2d import x2d
-from ..ocrreject import ocrreject
+stistools.r_util.NOT_APPLICABLE = 'n/a'  # Fix a bug in expandFileName()
+
+from stistools.r_util import expandFileName
+# from ..basic2d import basic2d
+# from ..x2d import x2d
+# from ..ocrreject import ocrreject
 
 
 def prepspec(inspec, outroot, darkfile=None, pixelflat=None, initguess='min'):
@@ -61,8 +67,64 @@ def prepspec(inspec, outroot, darkfile=None, pixelflat=None, initguess='min'):
     # If G750M:
     #    perform only {WAVECORR, HELCORR, X2DCORR} via stistools.x2d.x2d()
     #    return SX2 file
+    science_data = os.path.abspath(expandFileName(inspec))
+    sci_root = re.split('\.fits.*', os.path.basename(science_data), flags=re.IGNORECASE)[0].rsplit('_', 1)[0]
+    opt_elem = fits.getval(science_data, 'OPT_ELEM').strip().upper()
+    if (fits.getval(science_data, ext=0, keyword='DETECTOR').strip().upper() != 'CCD') or \
+            (fits.getval(science_data, ext=0, keyword='INSTRUME').strip() != 'STIS'):
+        raise Exception('prepspec:  Intended for use on STIS/CCD data!')
 
-    raise NotImplementedError()
+
+    # Make sure the necessary header keywords are set to PERFORM
+    fits.setval(science_data, 'BLEVCORR', value='PERFORM')  # overscan correction
+    fits.setval(science_data, 'CRCORR', value='PERFORM')  # cosmic ray rejection
+    fits.setval(science_data, 'BIASCORR', value='PERFORM')  # bias subtraction
+    fits.setval(science_data, 'DARKCORR', value='PERFORM')  # dark subtraction
+    fits.setval(science_data, 'FLATCORR', value='PERFORM')  # I believe this is the correct flat field correction keyword
+
+    # A few extra calibration steps if G750M data:
+    if opt_elem == 'G750M':
+        fits.setval(science_data, 'WAVECORR', value='PERFORM')  # wavelength calibration
+        fits.setcal(science_data, 'GEOCORR', value='PERFORM')  # geometric correction
+        fits.setval(science_data, 'X2DCORR', value='PERFORM')  # 2D rectification
+
+    if darkfile is not None:
+        # Use the input-specified dark file:
+        fits.setval(science_data, ext=0, keyword='DARKFILE', value=dark)
+    else:
+        darkfile = fits.getval(science_data, ext=0, keyword='DARKFILE')
+        print('Using DARKFILE from the header:\n    {}'.format(darkfile))
+    # dark = os.path.abspath(expandFileName(dark))  # Expand IRAF and UNIX $VARS
+
+    if pixelflat is not None:
+        # Use the input-specified pixel flat:
+        fits.setval(science_data, ext=0, keyword='PFLTFILE', value=pixelflat)
+    else:
+        pixelflat = fits.getval(science_data, ext=0, keyword='PFLTFILE')
+        print('Using PFLTFILE from the header:\n    {}'.format(pixelflat))
+    # pixflat = os.path.abspath(expandFileName(pixflat))  # Expand IRAF and UNIX $VARS
+
+    # Calibrate!
+    trl_file = '{}_trl.txt'.format(sci_root)  # Output log goes here
+    cwd = os.getcwd()
+    try:
+        # Run calstis from within the directory with the data to find EPC files properly:
+        os.chdir(os.path.dirname(science_data))
+        res = stistools.calstis.calstis(os.path.basename(science_data), trailer=trl_file)
+    finally:
+        os.chdir(cwd)
+
+    # Print out calstis log:
+    trl_file = os.path.join(os.path.dirname(science_data), trl_file)
+    with open(trl_file) as trl:
+        for line in trl:
+            print ('    ' + line.rstrip())
+
+    # Raise exception on bad calstis exit code:
+    if res != 0:
+        raise Exception('CalSTIS exited with code {}'.format(res))
+
+    # raise NotImplementedError()
 
 
 def call_prepspec():
