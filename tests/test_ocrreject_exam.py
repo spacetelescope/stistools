40ae575d34a7fd31a96666351b71cad48f718ed7
Matt Dallas <mdallas@stsci.edu>
2024-12-09 15:53:43 -0500

    Added beginning of testing infrastructure

---
+++ tests/test_ocrreject_exam.py
@@ -0,0 +1,56 @@
+from stistools.ocrreject_exam import ocrreject_exam
+from .resources import BaseSTIS
+import pytest
+
+
+@pytest.mark.bigdata
+@pytest.mark.slow
+class TestOcrrejectExam(BaseSTIS):
+
+    input_loc = 'ocrreject_exam'
+    ref_loc = 'ocrreject_exam/ref'
+
+    input_list = ["o58i01q7q_flt.fits", "o58i01q8q_flt.fits"]
+
+    # Make input file string
+    input_file_string = ", ".join(input_list)
+
+    def test_ocrrject_lev2(self):
+        """
+        This regression test for this level of this task is different than
+        level three in two ways.  Two parameters are set to give an initial
+        guess to the sky value and define a sky subraction method. It also
+        removes cosmic rays from 14 STIS/CCD images and creates a single
+        'clean' image which is compared to a reference file using 'FITSDIFF'.
+        """
+
+        # Prepare input files.
+        for filename in self.input_list:
+            self.get_input_file("input", filename)
+
+        # Run ocrreject
+        ocrreject(self.input_file_string, output="ocrreject_lev2_crj.fits",
+                  initgues="med", skysub="mode")
+
+        # Compare results
+        outputs = [("ocrreject_lev2_crj.fits", "ocrreject_lev2_crj_ref.fits")]
+        self.compare_outputs(outputs)
+
+    def test_ocrrject_lev3(self):
+        """
+        This regression test for this level on this task is a simple default
+        parameter execution of the task.  It attempts to remove cosmic rays
+        from 14 STIS/CCD images.  The resulting calibration is compared to a
+        reference file using 'FITSDIFF'.
+        """
+
+        # Prepare input files.
+        for filename in self.input_list:
+            self.get_input_file("input", filename)
+
+        # Run ocrreject
+        ocrreject(self.input_file_string, output="ocrreject_lev3_crj.fits")
+
+        # Compare results
+        outputs = [("ocrreject_lev3_crj.fits", "ocrreject_lev3_crj_ref.fits")]
+        self.compare_outputs(outputs)
