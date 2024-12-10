72609421d8f3c6e61d10b2c9beee60202f71a3d9
Matt Dallas <mdallas@stsci.edu>
2024-12-10 16:48:19 -0500

    Added test for occreject_exam

--- tests/test_ocrreject_exam.py
+++ tests/test_ocrreject_exam.py
@@ -1,5 +1,7 @@
 from stistools.ocrreject_exam import ocrreject_exam
 from .resources import BaseSTIS
+import numpy as np
+import os
 import pytest
 
 
@@ -8,7 +10,6 @@ import pytest
 class TestOcrrejectExam(BaseSTIS):
 
     input_loc = 'ocrreject_exam'
-    ref_loc = 'ocrreject_exam/ref'
 
     input_list = ["odvkl1040_flt.fits", "odvkl1040_sx1.fits"]
 
@@ -18,17 +19,28 @@ class TestOcrrejectExam(BaseSTIS):
     def test_ocrrject_exam(self):
         """
         This regression test runs the task on the known problematic dataset odvkl1040.
-        The resulting output dictionary  is compared to a
-        reference file using 'FITSDIFF'. COULD I JUST PULL OUT THE KEY/VAL PAIRS AND CHECK IF THEY'RE SIMILAR? 
+        The resulting output dictionary is compared to the known values for odvkl1040.
         """
 
         # Prepare input files.
         for filename in self.input_list:
-            self.get_input_file("input", filename)
+            local_file = self.get_data("input", filename)
+        
+        expected_output = {'rootname': 'odvkl1040',
+                            'extr_fracs': np.array([0.31530762, 0.32006836]),
+                            'outside_fracs': np.array([0.00884673, 0.00810278]), 
+                            'ratios': np.array([35.64113429, 39.50106762]),
+                            'avg_extr_frac': 0.31768798828125, 
+                            'avg_outside_frac': 0.008474755474901575,
+                            'avg_ratio': 37.486389928547126}
 
-        # Run ocrreject_exam
-        ocrreject(self.input_file_string, output="ocrreject_lev3_crj.fits")
+        resulting_output = ocrreject_exam('odvkl1040', data_dir=os.path.dirname(local_file))
 
-        # Compare results
-        outputs = [("ocrreject_lev3_crj.fits", "ocrreject_lev3_crj_ref.fits")]
-        self.compare_outputs(outputs)
+        assert len(resulting_output) == 1, "Output is not a list of only one dictionary"
+
+        resulting_output = resulting_output[0]
+
+        assert set(resulting_output) == set(expected_output), "Not all created keys match"
+
+        for key in expected_output:
+            assert resulting_output[key] == pytest.approx(expected_output[key]), f"{key} failed"
