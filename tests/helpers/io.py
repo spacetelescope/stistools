08306ded845e1d45315e2790a7e387c261368997
Sean Lockwood <lockwood@stsci.edu>
2025-01-02 14:04:28 -0500

    Cleaning up test infrastructure:   - Added ci-watson to pytest test dependency.     Install with "pip install '.[test]'" or "pip install 'stistools[test]'".   - Removed conftest.py; functionality implemented in ci-watson.   - Removed bigdata downloader; using ci-watson version.   - Removed download_crds functionality; using ci-watson version.

--- tests/helpers/io.py
+++ tests/helpers/io.py
@@ -3,8 +3,6 @@ import json
 import os
 import shutil
 
-from .utils import check_url, download
-
 UPLOAD_SCHEMA = {"files": [
                     {"pattern": "",
                      "target": "",
@@ -18,55 +16,7 @@ UPLOAD_SCHEMA = {"files": [
                   ]
                 }
 
-__all__ = ['BigdataError', 'get_bigdata', 'upload_results']
-
-
-BIGDATA_PATHS = [
-    os.environ.get('TEST_BIGDATA', '/srv/rt/betadrizzle'),
-    'https://bytesalad.stsci.edu/artifactory/datb-stistools'
-]
-
-
-class BigdataError(Exception):
-    pass
-
-
-def _select_bigdata():
-    """ Find and returns the path to the nearest big datasets
-    """
-    for path in BIGDATA_PATHS:
-        if os.path.exists(path) or check_url(path):
-            return path
-
-    return None
-
-
-def get_bigdata(*args):
-    """ Acquire requested data from a managed resource
-    Usage:
-        filename = get_bigdata('abc', '123', 'sample.fits')
-        with open(filename, 'rb') as data:
-            example = data.read()
-    Returns:
-        Absolute path to local copy of data (i.e. /path/to/example.fits)
-    """
-    src = os.path.join(_select_bigdata(), *args)
-    filename = os.path.basename(src)
-    dest = os.path.abspath(os.path.join(os.curdir, filename))
-
-    if os.path.exists(src):
-        if src == dest:
-            raise BigdataError('Source and destination paths are identical: '
-                               '{}'.format(src))
-        shutil.copy2(src, dest)
-
-    elif check_url(src):
-        download(src, dest)
-
-    else:
-        raise BigdataError('Failed to retrieve data: {}'.format(src))
-
-    return dest
+__all__ = ['upload_results',]
 
 
 def upload_results(**kwargs):
