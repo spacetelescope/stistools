fixed tag file re-stuff issue which uncovered a bug in the finite-distance calculation (now fixed)

stistools/barycentric_correction.py
@@ -181,10 +181,18 @@ def barycentric_correction(table_names, verbose=True, distance=1e9,
         else:
             in_hdul[0].header['DELAYCOR'] = "PERFORM"
             
-        #if we decide to make compatable with COS, they have no TEXPSTRT
-        #if in_hdul[0].header['INSTRUME'] == "STIS": 
-        mjd1 = in_hdul[0].header['TEXPSTRT']
-        mjd2 = in_hdul[0].header['TEXPEND']
+        # COS has no TEXPSTRT in primary header, so use EXPSTART in first ext
+        if in_hdul[0].header['INSTRUME'] == "STIS": 
+            mjd1 = in_hdul[0].header['TEXPSTRT']
+            mjd2 = in_hdul[0].header['TEXPEND']
+        elif in_hdul[0].header['INSTRUME'] == "COS": 
+            mjd1 = in_hdul[1].header['EXPSTART']
+            mjd2 = in_hdul[1].header['EXPEND']
+        else:
+            print('baycentric_correction only works with STIS and COS files.')
+            raise(ValueError)
+            #return
+            
         ra = in_hdul[0].header['RA_TARG']
         dec = in_hdul[0].header['DEC_TARG']
         
@@ -283,11 +291,13 @@ def barycentric_correction(table_names, verbose=True, distance=1e9,
             else:
                 delta_sec = calc_delay_orbfile(epoch_array, ra, dec, hst_orb, distance=distance)
                 
+            # This is correcting for the difference between the new time interval
+            # and the *corrected* exposure start time 
+            # (i.e., the add'l correction now that the Earth and HST have moved)
             time_array = time_array + (delta_sec - t0_delay).value*SECPERDAY
         
-        
-            # re-stuff time array
-            #in_hdul['EVENTS', e_indx].data[in_col] = time_array
+            # replace time array <- wasn't being restuffed before
+            in_hdul['EVENTS', e_indx].data[in_col] = time_array
         
             # add delaytime to EXPSTART and EXPEND, and update header
             if hst_orb is None:
@@ -346,22 +356,27 @@ def barycentric_correction(table_names, verbose=True, distance=1e9,
         if time_script:
             tcheck2 = time.time()
             print(f'Checkpoint 2: {tcheck2-tstart} s')
-        
+            
+        # IF STIS
         # add delaytime to TEXPSTRT and TEXPEND, and update primary header
-        mjd1 = in_hdul[0].header['TEXPSTRT']
-        if hst_orb is None:
-            delta_sec = calc_delay_jpl(mjd1, ra, dec, distance=distance)
-        else:
-            delta_sec = calc_delay_orbfile(mjd1, ra, dec, hst_orb, distance=distance)
-        
-        in_hdul[0].header['TEXPSTRT'] = mjd1 + delta_sec.value
-        
-        mjd2 = in_hdul[0].header['TEXPEND']
-        if hst_orb is None:
-            delta_sec = calc_delay_jpl(mjd2, ra, dec, distance=distance)
-        else:
-            delta_sec = calc_delay_orbfile(mjd2, ra, dec, hst_orb, distance=distance)
-        in_hdul[0].header['TEXPEND'] = mjd2 + delta_sec.value
+        # COS has no TEXPSTRT in primary header
+        if in_hdul[0].header['INSTRUME'] == "STIS": 
+            mjd1 = in_hdul[0].header['TEXPSTRT']
+            if hst_orb is None:
+                delta_sec = calc_delay_jpl(mjd1, ra, dec, distance=distance)
+            else:
+                delta_sec = calc_delay_orbfile(mjd1, ra, dec, hst_orb, 
+                                               distance=distance)
+            
+            in_hdul[0].header['TEXPSTRT'] = mjd1 + delta_sec.value
+            
+            mjd2 = in_hdul[0].header['TEXPEND']
+            if hst_orb is None:
+                delta_sec = calc_delay_jpl(mjd2, ra, dec, distance=distance)
+            else:
+                delta_sec = calc_delay_orbfile(mjd2, ra, dec, hst_orb, 
+                                               distance=distance)
+            in_hdul[0].header['TEXPEND'] = mjd2 + delta_sec.value
         
         # add keyword to flag the fact that the times have been corrected
         in_hdul[0].header['DELAYCOR'] = ("COMPLETE",
@@ -574,10 +589,28 @@ def calc_delay_jpl(times, ra, dec, distance=1e9, verbose=True):
                       target.cartesian.z.value]
     
         # Calculate the finite-distance correction term
+        # correction_term = []
+        # for j in range(shape(hstbary)[1]):
+        #     hstbary_tmp = np.array([hstbary[0][j],hstbary[1][j],hstbary[2][j]])
+        #     correction_tmp = ((-0.5/distance) *
+        #                        (np.sum((np.array(hstbary_tmp))**2) -
+        #                         (np.dot(target_arr,hstbary_tmp))**2)*u.AU /
+        #                        c.c).to('day')
+        #     correction_term.append(correction_tmp.value)
+        # correction_term*u.day
+            
+        # This is wrong when hstbary is multiple points!
+        # The more points querried, the larger the correction lmaoooo
+        #correction_term = ((-0.5/distance) *
+        #                   (np.sum((np.array(hstbary))**2) -
+        #                    (np.dot(target_arr,hstbary))**2)*u.AU /
+        #                   c.c).to('day') 
+        
+        # Actually just need to make the above sum over the correction axis
         correction_term = ((-0.5/distance) *
-                           (np.sum((np.array(hstbary))**2) -
+                          (np.sum((np.array(hstbary))**2, axis=0) -
                             (np.dot(target_arr,hstbary))**2)*u.AU /
-                           c.c).to('day')
+                          c.c).to('day') 
         
         if verbose:
             if correction_term.size < 10:
@@ -852,8 +885,12 @@ def odelay_file_compare(file1, file2, in_col = 'TIME'):
     x = fits.open(file1)
     x2 = fits.open(file2)
     
-    #print(f"TEXPSTRT file2-file1: {x2[0].header['TEXPSTRT']-x[0].header['TEXPSTRT']} days")
-    print(f"TEXPSTRT file2-file1 {(x2[0].header['TEXPSTRT']-x[0].header['TEXPSTRT'])*24*60*60} seconds")
+    # IF STIS
+    # add delaytime to TEXPSTRT and TEXPEND, and update primary header
+    # COS has no TEXPSTRT in primary header
+    if x[0].header['INSTRUME'] == "STIS": 
+        #print(f"TEXPSTRT file2-file1: {x2[0].header['TEXPSTRT']-x[0].header['TEXPSTRT']} days")
+        print(f"TEXPSTRT file2-file1 {(x2[0].header['TEXPSTRT']-x[0].header['TEXPSTRT'])*24*60*60} seconds")
 
     
     #print(f"EXPSTART file2-file1 {x2[1].header['EXPSTART']-x[1].header['EXPSTART']} days")
@@ -865,7 +902,10 @@ def odelay_file_compare(file1, file2, in_col = 'TIME'):
         
         print(f"Last TIME file2-file1: {x2[1].data[in_col][-1]-x[1].data[in_col][-1]} seconds")
 
-    t = T.Time(x[0].header['TEXPSTRT'],format='mjd',scale='utc')
+    if x[0].header['INSTRUME'] == "STIS": 
+        t = T.Time(x[0].header['TEXPSTRT'],format='mjd',scale='utc')
+    if x[0].header['INSTRUME'] == "COS": 
+        t = T.Time(x[1].header['EXPSTART'],format='mjd',scale='utc')
     diff = (t.tdb.value - t.tt.value)*24*60*60
     print(f'In case it is helpful, the difference between TT and TDB_BJD is {diff:.6f} s')
     