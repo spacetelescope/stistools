c08595006fb4852240dff644ec54b8185824b33e
Sean Lockwood <lockwood@stsci.edu>
2026-06-23 16:09:54 -0400

    Reorganized splice and calculate_sensitivity into a single module.

---
+++ stistools/splice/__init__.py
@@ -0,0 +1,16 @@
+from .splice import (
+    splice,
+    read_spectrum,
+    find_overlap,
+    merge_overlap,
+    concatenate_sections,
+    nearest_index,
+    __doc__ as __doc_splice__)
+from .calculate_sensitivity import (
+    calculate_sensitivity,
+    eval_tds,
+    extraction_height_correction,
+    aperture_throughput,
+    __doc__ as __doc_calculate_sensitivity__)
+
+__doc__ = __doc_splice__ + '\n\n' + __doc_calculate_sensitivity__
